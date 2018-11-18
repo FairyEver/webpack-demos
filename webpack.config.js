8e15269be4b2c783ea35e1204481443b80d15004
liyang <1711467488@qq.com>
2018-11-18 20:54:12 +0800

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,27 @@
+const path = require('path')
+
 module.exports = {
-  //
+
+  // 单输入 单输出
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   filename: 'bundle.js'
+  // }
+
+  // 多入口 多输出
+  // entry: {
+  //   1: './src/1.js',
+  //   2: './src/2.js',
+  // },
+  // output: {
+  //   filename: '[name].js',
+  //   path: path.resolve(__dirname, 'dist')
+  // }
+
+  // 输出带哈希值
+  output: {
+    filename: '[name]-[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+
 }
